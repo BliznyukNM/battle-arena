73a246165675090f816f3aae2856062142a41b9b
Nikita Blizniuk <bliznyuknm@gmail.com>
2023-04-10 22:33:23 +0200

    camera follows cursor

--- battle-arena/map/Camera.gd
+++ battle-arena/map/Camera.gd
@@ -1,14 +1,22 @@
 extends Camera3D
 
 
-@export var lerp_speed: float = 2
-@export var offset: Vector3
 @export var target: Node3D:
     set(value):
         target = value
         if target: position = target.position + offset
 
+@export var lerp_speed: float = 2
+@export var offset: Vector3
+@export var max_distance: float = 30
+
+
+@onready var input: PlayerInput = target.get_node("Input")
+
 
 func _process(delta: float) -> void:
     if not target: return
-    position = lerp(position, target.position + offset, delta * lerp_speed)
+    
+    var look_at_pos: = input.look_at_point
+    var shift: = (look_at_pos - target.position).limit_length(max_distance)
+    position = lerp(position, target.position + shift + offset, delta * lerp_speed)
