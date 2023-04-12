40e3996eae005e8d59e52ab4ed84ee560fe02ef2
Nikita Blizniuk <bliznyuknm@gmail.com>
2023-04-12 19:40:42 +0200

    fixed ranged dummy

--- battle-arena/characters/dummy/shooting_dummy.gd
+++ battle-arena/characters/dummy/shooting_dummy.gd
@@ -1,14 +1,9 @@
 extends Node3D
 
 
-@onready var fireball: = preload("res://characters/dummy/fireball.tscn")
+@onready var skill: BaseSkill = $RangeAttack
 
 
-func _on_shoot() -> void:
-    if not multiplayer.is_server(): return
-    var fireball_instance: = fireball.instantiate()
-    fireball_instance.position = global_position + Vector3(0, 1, 1)
-    fireball_instance.set_team(0)
-    fireball_instance.set_attack_number(0)
-    fireball_instance.throw_direction(Vector3.BACK)
-    $MultiplayerSpawner.add_child(fireball_instance, true)
+func _process(delta: float) -> void:
+    if not skill.cooldown.is_stopped(): return
+    skill.activate(true)
