2b4c040075231a3b80c5eacddc604dbf2e1c2103
Nikita Blizniuk <bliznyuknm@gmail.com>
2023-04-13 04:02:08 +0200

    skills applying movement and rotation slowdown

--- battle-arena/characters/rotation.gd
+++ battle-arena/characters/rotation.gd
@@ -10,7 +10,7 @@ extends Node
 func _process(delta: float) -> void:
     var new_basis: Basis = Basis.looking_at(owner.position - input.look_at_point)
     var weight = delta * speed.current_value
-    owner.transform.basis = lerp(owner.transform.basis, new_basis, weight)
+    owner.transform.basis = lerp(owner.transform.basis, new_basis, clampf(weight, 0, 1))
     
     owner.rotation.x = 0.0
     owner.rotation.z = 0.0
--- battle-arena/skills/base_attack_skill.gd
+++ battle-arena/skills/base_attack_skill.gd
@@ -5,14 +5,23 @@ extends "res://skills/base_skill.gd"
 @export var hit_time: float
 
 
+@onready var slowdown_effect: BaseStatusEffect = preload("res://status_effects/slowdown.tres").duplicate()
+@onready var slow_rotation_effect: BaseStatusEffect = preload("res://status_effects/slow_rotation.tres").duplicate()
+
+
 func _ready() -> void:
     assert(cooldown.time_left >= execution.time_left, \
         "Execution time have to be bigger or equal than cooldown.")
     super._ready()
+    slowdown_effect.time = execution.wait_time
+    slow_rotation_effect.time = execution.wait_time
 
 
 func _on_pressed() -> void:
     super._on_pressed()
+    
+    %StatusEffects.add_status_effect(slowdown_effect)
+    %StatusEffects.add_status_effect(slow_rotation_effect)
     
     var tween: = create_tween()
     tween.tween_callback(self._try_hit).set_delay(hit_time)
