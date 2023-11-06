7ed2336295b38db5dbedd2d02a637b3747ae8d91
Nikita Blizniuk <bliznyuknm@gmail.com>
2023-11-06 12:20:25 +0100

    integrated itch.io authorisation

--- battle-arena/lobby/login.gd
+++ battle-arena/lobby/login.gd
@@ -12,7 +12,8 @@ const CONFIG_FILE: = "user://local.ini"
 
 
 func _ready() -> void:
-    _restore()
+    if OS.has_feature("editor"): _restore()
+    else: _try_login_itch()
 
 
 func on_login_pressed() -> void:
@@ -43,6 +44,27 @@ func _restore() -> void:
     
     $Nickname.text = _config.get_value("auth", "login", "")
     _uuid = _config.get_value("auth", "uuid", UUID.v4())
+
+
+func _try_login_itch() -> void:
+    var api_key: = OS.get_environment("ITCHIO_API_KEY")
+    
+    var httpRequest: = HTTPRequest.new()
+    add_child(httpRequest)
+    httpRequest.request_completed.connect(_on_itch_request_completed)
+    
+    var headers: = ["Authorization: %s" % api_key]
+    
+    var error = httpRequest.request("https://itch.io/api/1/jwt/me", headers, HTTPClient.METHOD_GET)
+    if error != OK:
+        push_error("An error occurred in the HTTP request.")
+
+
+func _on_itch_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray) -> void:
+    var json = JSON.parse_string(body.get_string_from_utf8())
+    $Nickname.text = json.user.username
+    var auth_result: = await Matchmaking.authenticate(json.user.id, json.user.username, true)
+    if auth_result: loader.start_home()
 
 
 func _save_username() -> void:
