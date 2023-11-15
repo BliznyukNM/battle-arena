49515f6389216e42c7da79b0818162c95d6d2b9c
Nikita Blizniuk <bliznyuknm@gmail.com>
2023-11-15 23:01:50 +0100

    fixed ranger's trap, uploader versioning

--- setup.sh
+++ setup.sh
@@ -2,3 +2,12 @@
 mkdir battle-arena/addons
 ln -s ../../nakama-godot/addons/com.heroiclabs.nakama battle-arena/addons/com.heroiclabs.nakama
 ln -s ../../Gut/addons/gut battle-arena/addons/gut
+
+mkdir builds
+mkdir builds/windows
+mkdir builds/macos
+mkdir builds/linux
+
+cp .itch.toml builds/windows
+cp .itch.toml builds/macos
+cp .itch.toml builds/linux
--- uploader.sh
+++ uploader.sh
@@ -1,13 +1,13 @@
 windows() {
-  butler push builds/windows bliznyuknm/battle-arena:windows
+  butler push builds/windows bliznyuknm/battle-arena:windows --userversion-file=version.txt
 }
 
 linux() {
-  butler push builds/linux bliznyuknm/battle-arena:linux
+  butler push builds/linux bliznyuknm/battle-arena:linux --userversion-file=version.txt
 }
 
 macos() {
-  butler push builds/macos bliznyuknm/battle-arena:osx-universal
+  butler push builds/macos bliznyuknm/battle-arena:osx-universal --userversion-file=version.txt
 }
 
 all () {
