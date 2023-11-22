93a996c9b6eb17c95b022e8b72b995621ca76534
Nikita Blizniuk <bliznyuknm@gmail.com>
2023-11-22 11:42:34 +0100

    fixed dummy shooting

--- battle-arena/characters/dummy/shooting_dummy_input_source.gd
+++ battle-arena/characters/dummy/shooting_dummy_input_source.gd
@@ -4,3 +4,7 @@ extends InputSource
 func is_basic_attack_just_pressed() -> bool:
     var basic_attack = _owner.skills.basic_attack
     return basic_attack.execution.is_stopped() and basic_attack.cooldown.is_stopped()
+
+
+func get_look_at_point() -> Vector3:
+    return _owner.position + _owner.transform.basis.z
