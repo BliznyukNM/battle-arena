7cf24e927f8d4a70c5f21d4939dcb61524abb550
Nikita Blizniuk <bliznyuknm@gmail.com>
2024-03-12 12:25:57 +0100

    removed all skills from ranger

--- battle-arena/characters/skills/skill.gd
+++ battle-arena/characters/skills/skill.gd
@@ -3,19 +3,19 @@ extends BeehaveTree
 
 
 var execution: float:
-    get: return blackboard.get_value("execution", 0.0, self.root_node.name)
+    get: return blackboard.get_value("execution", 0.0, self.root_name)
 
 var cooldown: float:
-    get: return blackboard.get_value("cooldown", 0.0, self.root_node.name )
+    get: return blackboard.get_value("cooldown", 0.0, self.root_name)
 
 var title: String:
-    get: return tr(self.root_node.name)
+    get: return tr(self.root_name)
 
 var description: String:
-    get: return tr("%s_desc" % self.root_node.name)
+    get: return tr("%s_desc" % self.root_name)
 
-var root_node: BeehaveNode:
+var root_name: String:
     get:
         var running_action: BeehaveNode = blackboard.get_value("running_action", null, str(actor.get_instance_id()))
-        if not running_action: return get_child(0)
-        return running_action.get_parent()
+        if not running_action: return ""
+        return running_action.get_parent().name
