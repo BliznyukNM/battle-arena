1e5e91f55bef19aad5a819c141c1db6307a685ad
Nikita Blizniuk <bliznyuknm@gmail.com>
2024-03-13 09:46:47 +0100

    added authority decorator

---
+++ battle-arena/characters/skills/decorators/empty.gd
@@ -0,0 +1,30 @@
+@tool
+class_name EmptyDecorator extends Decorator
+
+
+func tick(actor: Node, blackboard: Blackboard) -> int:
+    var c = get_child(0)
+
+    if c != running_child:
+        c.before_run(actor, blackboard)
+
+    var response = c.tick(actor, blackboard)
+    if can_send_message(blackboard):
+        BeehaveDebuggerMessages.process_tick(c.get_instance_id(), response)
+
+    if c is ConditionLeaf:
+        blackboard.set_value("last_condition", c, str(actor.get_instance_id()))
+        blackboard.set_value("last_condition_status", response, str(actor.get_instance_id()))
+
+    match response:
+        SUCCESS, FAILURE:
+            c.after_run(actor, blackboard)
+            return response
+        RUNNING:
+            running_child = c
+            if c is ActionLeaf:
+                blackboard.set_value("running_action", c, str(actor.get_instance_id()))
+            return RUNNING
+        _:
+            push_error("This should be unreachable")
+            return -1
