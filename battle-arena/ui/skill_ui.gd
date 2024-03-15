cfd7cf9c58a82f2dd1cecb6b695378b04ec34a74
Danila <danila.blizniuk@gmail.com>
2024-03-15 16:49:56 +0300

    Icons for barbarian

--- battle-arena/ui/skill_ui.gd
+++ battle-arena/ui/skill_ui.gd
@@ -1,4 +1,4 @@
-extends Label
+extends Node
 
 
 @onready var cooldown: Label = $Cooldown
@@ -8,14 +8,14 @@ var _skill
 
 
 func register(skill) -> void:
-    _skill = skill
+	_skill = skill
 
 
 func _process(delta: float) -> void:
-    if not _skill: return
-    
-    cooldown.visible = _skill.cooldown > 0.0
-    cooldown.text = "%1.2f" % _skill.cooldown
-    
-    var enabled: bool = _skill.enabled and _skill.cooldown <= 0.0
-    add_theme_color_override("font_color", Color.WHITE if enabled else Color.GRAY) 
+	if not _skill: return
+	
+	cooldown.visible = _skill.cooldown > 0.0
+	cooldown.text = "%1.2f" % _skill.cooldown
+	
+	#var enabled: bool = _skill.enabled and _skill.cooldown <= 0.0
+	#add_theme_color_override("font_color", Color.WHITE if enabled else Color.GRAY) 
