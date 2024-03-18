c84baa4ebc5acf91e74880cb95161bd3ed07f275
Danila <danila.blizniuk@gmail.com>
2024-03-18 16:35:43 +0300

    Fixed indentation

--- battle-arena/ui/gui.gd
+++ battle-arena/ui/gui.gd
@@ -2,16 +2,16 @@ extends Control
 
 
 var target: Node:
-	set(value):
-		target = value
-		_update_hud.call_deferred()
+    set(value):
+        target = value
+        _update_hud.call_deferred()
 
 
 func _update_hud() -> void:
-	$BottomHUD/LMB_button.register(target.skills.basic_attack)
-	#$BottomHUD/RMB_button.register(target.skills.secondary_attack)
-	#$BottomHUD/Space_button.register(target.skills.third_attack)
-	#$BottomHUD/Q_button.register(target.skills.block)
-	#$BottomHUD/E_button.register(target.skills.dodge)
-	#$BottomHUD/R_button.register(target.skills.ultimate)
-	$ID.text = "ID: %d" % target.player_id
+    $BottomHUD/LMB_button.register(target.skills.basic_attack)
+    #$BottomHUD/RMB_button.register(target.skills.secondary_attack)
+    #$BottomHUD/Space_button.register(target.skills.third_attack)
+    #$BottomHUD/Q_button.register(target.skills.block)
+    #$BottomHUD/E_button.register(target.skills.dodge)
+    #$BottomHUD/R_button.register(target.skills.ultimate)
+    $ID.text = "ID: %d" % target.player_id
--- battle-arena/ui/skill_ui.gd
+++ battle-arena/ui/skill_ui.gd
@@ -9,15 +9,15 @@ var _skill
 
 
 func register(skill) -> void:
-	_skill = skill
-	texture = skill.icon
+    _skill = skill
+    texture = skill.icon
 
 
 func _process(delta: float) -> void:
-	if not _skill: return
-	
-	cooldown.visible = _skill.cooldown > 0.0
-	cooldown.text = "%1.2f" % _skill.cooldown
-	
-	#var enabled: bool = _skill.enabled and _skill.cooldown <= 0.0
-	#add_theme_color_override("font_color", Color.WHITE if enabled else Color.GRAY) 
+    if not _skill: return
+    
+    cooldown.visible = _skill.cooldown > 0.0
+    cooldown.text = "%1.2f" % _skill.cooldown
+    
+    #var enabled: bool = _skill.enabled and _skill.cooldown <= 0.0
+    #add_theme_color_override("font_color", Color.WHITE if enabled else Color.GRAY) 
