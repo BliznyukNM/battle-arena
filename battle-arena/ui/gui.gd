15cb9b13f0ed389802ae97689e10318b103f1d2c
Danila <danila.blizniuk@gmail.com>
2024-04-03 20:42:33 +0300

    New dynamic barbarian icons

--- battle-arena/ui/gui.gd
+++ battle-arena/ui/gui.gd
@@ -7,14 +7,14 @@ var target: Character:
 func _update_hud() -> void:
     $BottomHUD/LMB_button.register(target.skills.basic_attack)
     $BottomHUD/RMB_button.register(target.skills.second_attack)
-    $BottomHUD/Space_button.register(target.skills.third_attack)
+    $BottomHUD/E_button.register(target.skills.third_attack)
     $BottomHUD/Q_button.register(target.skills.block)
-    $BottomHUD/E_button.register(target.skills.dodge)
+    $BottomHUD/Space_button.register(target.skills.dodge)
     $BottomHUD/R_button.register(target.skills.ultimate)
     $ID.text = "ID: %d" % target.player_id
 
 
 func _process(_delta: float) -> void:
     if not target: return
-    _update_hud()
+    _update_hud.call_deferred()
         
--- battle-arena/ui/skill_ui.gd
+++ battle-arena/ui/skill_ui.gd
@@ -1,4 +1,4 @@
-extends Sprite2D
+extends TextureRect
 
 
 @onready var cooldown: Label = $Cooldown
@@ -6,12 +6,12 @@ extends Sprite2D
 
 var _skill
 
-
+#func _ready() -> void:
+    #scale -= Vector2(2.78, 2.78)
 
 func register(skill) -> void:
     _skill = skill
     texture = Icons.get_icon(skill.root_name)
-    
 
 
 func _process(delta: float) -> void:
@@ -20,6 +20,6 @@ func _process(delta: float) -> void:
     cooldown.visible = not is_zero_approx(_skill.cooldown)
     cooldown.text = "%1.2f" % _skill.cooldown
     
-    var enabled: bool = _skill.enabled and is_zero_approx(_skill.cooldown) and is_zero_approx(_skill.execution)
-    add_theme_color_override("font_color", Color.WHITE if enabled else Color.GRAY)
+    #var enabled: bool = _skill.enabled and is_zero_approx(_skill.cooldown) and is_zero_approx(_skill.execution)
+    #add_theme_color_override("font_color", Color.WHITE if enabled else Color.GRAY)
 
