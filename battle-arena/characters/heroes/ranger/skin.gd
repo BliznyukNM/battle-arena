264332288365e981841adf20f14c7df9c395f1a7
Nikita Blizniuk <bliznyuknm@gmail.com>
2024-04-08 22:07:28 +0200

    consume energy leaf

--- battle-arena/characters/heroes/ranger/skin.gd
+++ battle-arena/characters/heroes/ranger/skin.gd
@@ -8,18 +8,6 @@ extends "res://characters/skin.gd"
 @export var ultimate: Material
 
 
-func update_move_direction(direction: Vector2) -> void:
-    animationTree.set("parameters/Movement/blend_position", direction)
-
-
-func update_move_speed(speed: float) -> void:
-    animationTree.set("parameters/MoveSpeed/scale", speed)
-
-
-func update_attack_speed(speed: float) -> void:
-    animationTree.set("parameters/AttackSpeed/scale", speed)
-
-
 func play_shoot() -> void:
     animationTree.set("parameters/PlayShoot/request", AnimationNodeOneShot.ONE_SHOT_REQUEST_FIRE)
 
