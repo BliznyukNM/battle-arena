3eab20c1f59688286354d42167c1be94b32385ef
Nikita Blizniuk <bliznyuknm@gmail.com>
2024-04-11 14:41:18 +0200

    ranger ultimate preview

--- battle-arena/characters/skills/preview/cone_preview.gd
+++ battle-arena/characters/skills/preview/cone_preview.gd
@@ -6,6 +6,8 @@ extends "res://characters/skills/preview/base_preview.gd"
 
 func _construct_mesh() -> Mesh:
     var options: Dictionary = _get_options.call()
-    var radius: float = options.get("radius", 1.0)
+    var radius: float = options.get("distance", 0.0)
+    var angle: float = options.get("angle", 60.0)
     _view.scale = Vector3.ONE * radius * 2
+    _mesh.surface_get_material(0).set_shader_parameter("angle", float(angle))
     return _mesh
